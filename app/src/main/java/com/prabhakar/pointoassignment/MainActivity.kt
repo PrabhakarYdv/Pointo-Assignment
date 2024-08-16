59694dfe0a2fe1938eea4e503537750680d56e5b
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-08-16 19:03:20 +0530

    Initial commit

---
+++ app/src/main/java/com/prabhakar/pointoassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prabhakar.pointoassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

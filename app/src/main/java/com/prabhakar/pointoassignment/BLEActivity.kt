02f20c15ac49402c4afcbb3968958977a7b4df0d
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-08-18 08:56:31 +0530

    Bug Fix BLE

--- app/src/main/java/com/prabhakar/pointoassignment/BLEActivity.kt
+++ app/src/main/java/com/prabhakar/pointoassignment/BLEActivity.kt
@@ -1,19 +1,24 @@
 package com.prabhakar.pointoassignment
 
 import android.Manifest
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
-import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.prabhakar.pointoassignment.databinding.ActivityBleactivityBinding
@@ -21,164 +26,171 @@ import com.prabhakar.pointoassignment.databinding.ActivityBleactivityBinding
 class BLEActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBleactivityBinding
     private lateinit var bluetoothAdapter: BluetoothAdapter
-    private var bluetoothGatt: BluetoothGatt? = null
-    private lateinit var scanCallback: ScanCallback
-    lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private var bluetoothLeScanner: BluetoothLeScanner? = null
+    private var scanCallback: ScanCallback? = null
+    private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
 
+    private val requestBluetoothPermissions = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (permissions[android.Manifest.permission.BLUETOOTH_CONNECT] == true &&
+            permissions[android.Manifest.permission.BLUETOOTH] == true &&
+            permissions[android.Manifest.permission.BLUETOOTH_SCAN] == true &&
+            permissions[android.Manifest.permission.BLUETOOTH_ADMIN] == true &&
+            permissions[android.Manifest.permission.ACCESS_FINE_LOCATION] == true
+        ) {
+            startScanningForDevices()
+        } else {
+            requestPermissions()
+            Toast.makeText(this, "Permissions required for BLE", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBleactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothAdapter = bluetoothManager.adapter
+        bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        bluetoothLeScanner = bluetoothAdapter?.bluetoothLeScanner
 
-        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                startScanningForDevices()
+            } else {
 
+                Toast.makeText(
+                    this,
+                    "Bluetooth is required to scan for devices",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+        if (!bluetoothAdapter.isEnabled || bluetoothAdapter == null) {
+            val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            enableBluetoothLauncher.launch(intent)
+        } else {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                requestPermissions()
+            }
+            if (scanCallback != null) {
+                bluetoothLeScanner?.startScan(scanCallback)
+            }
+        }
+
+        requestPermissions()
         binding.btnScan.setOnClickListener {
-            scanDevice()
+            requestPermissions()
+            startScanningForDevices()
         }
     }
 
 
-    private fun scanDevice() {
+    private fun requestPermissions() {
+        requestBluetoothPermissions.launch(
+            arrayOf(
+                android.Manifest.permission.BLUETOOTH_CONNECT,
+                android.Manifest.permission.BLUETOOTH,
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_ADMIN,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+
+
+    private fun startScanningForDevices() {
         scanCallback = object : ScanCallback() {
-            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
                 super.onScanResult(callbackType, result)
-
-                runOnUiThread {
-                    binding.apply {
-                        if (ActivityCompat.checkSelfPermission(
-                                applicationContext,
-                                Manifest.permission.BLUETOOTH_CONNECT
-                            ) != PackageManager.PERMISSION_GRANTED
-                        ) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                                ActivityCompat.requestPermissions(
-                                    this@BLEActivity,
-                                    arrayOf(
-                                        Manifest.permission.BLUETOOTH_CONNECT
-                                    ),
-                                    1002
-                                )
-                            }
-
-                        } else {
-                            deviceName.text = "Device Name : ${result?.device?.name}"
-                            deviceAddress.text = "Device Address : ${result?.device?.address}"
-                        }
-
-                    }
-                }
-
-                bluetoothGatt = result?.device?.connectGatt(this@BLEActivity, false, gattCallback)
-            }
-
-            override fun onBatchScanResults(results: MutableList<ScanResult>?) {
-                super.onBatchScanResults(results)
-                for (result in results!!) {
-                    onScanResult(ScanSettings.CALLBACK_TYPE_ALL_MATCHES, result)
-                }
-            }
-
-            override fun onScanFailed(errorCode: Int) {
-                super.onScanFailed(errorCode)
-                runOnUiThread {
-                    Toast.makeText(
+                val device = result.device
+                if (ActivityCompat.checkSelfPermission(
                         applicationContext,
-                        "Scan Failed : $errorCode",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                }
-            }
-        }
-        bluetoothLeScanner.startScan(scanCallback)
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions()
 
-
-    }
-
-    private var gattCallback = object : BluetoothGattCallback() {
-        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-            super.onConnectionStateChange(gatt, status, newState)
-            if (newState == BluetoothGatt.STATE_CONNECTED) {
-                runOnUiThread {
-                    if (ActivityCompat.checkSelfPermission(
-                            applicationContext,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                            ActivityCompat.requestPermissions(
-                                this@BLEActivity,
-                                arrayOf(
-                                    Manifest.permission.BLUETOOTH_CONNECT
-                                ),
-                                1002
-                            )
-                        }
-
-                    } else {
-                        Toast.makeText(
-                            applicationContext,
-                            "Connected to ${gatt?.device?.name}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-            }
-        }
-
-
-        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-            super.onServicesDiscovered(gatt, status)
-            runOnUiThread {
-                binding.apply {
-                    if (ActivityCompat.checkSelfPermission(
-                            applicationContext,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                            ActivityCompat.requestPermissions(
-                                this@BLEActivity,
-                                arrayOf(
-                                    Manifest.permission.BLUETOOTH_CONNECT
-                                ),
-                                1002
-                            )
-                        }
-
-                    } else {
-                        for (service in gatt?.services!!) {
-                            deviceService.text = "Device Service : ${service.uuid}"
-                            for (characteristic in service.characteristics) {
-                                deviceCharacteristics.text =
-                                    "Device Characteristics :${characteristic.uuid}"
-                            }
+                } else {
+                    connectToDevice(device)
+                    Log.d("BLE", "Device found: ${device.name}, ${device.address}")
+                    runOnUiThread {
+                        binding.apply {
+                            deviceName.text = "Device Name: ${device.name}"
+                            deviceAddress.text = "Device Address: ${device.address}"
                         }
                     }
-
                 }
             }
-
         }
 
+        bluetoothLeScanner?.startScan(scanCallback)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    private fun stopScanningForDevices(scanCallback: ScanCallback) {
         if (ActivityCompat.checkSelfPermission(
                 this,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) == PackageManager.PERMISSION_GRANTED
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-            bluetoothGatt?.close()
-            bluetoothGatt = null
+
+            return
         }
-        bluetoothAdapter.bluetoothLeScanner.stopScan(scanCallback)
+        bluetoothLeScanner?.stopScan(scanCallback)
+    }
+
+    private fun connectToDevice(device: BluetoothDevice) {
+        val gattCallback = object : BluetoothGattCallback() {
+            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+                super.onConnectionStateChange(gatt, status, newState)
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    if (ActivityCompat.checkSelfPermission(
+                            applicationContext,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        requestPermissions()
+                    }
+                    Log.d("BLE", "Connected to ${device.name}")
+                    gatt.discoverServices()
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.d("BLE", "Disconnected from ${device.name}")
+                }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+                super.onServicesDiscovered(gatt, status)
+                for (service in gatt.services) {
+                    Log.d("BLE", "Service discovered: ${service.uuid}")
+                    runOnUiThread {
+                        binding.apply {
+                            deviceService.text = "Device Service: ${service.uuid}"
+                        }
+                    }
+                    for (characteristic in service.characteristics) {
+                        Log.d("BLE", "Service discovered: ${characteristic.uuid}")
+                        runOnUiThread {
+                            binding.apply {
+                                deviceCharacteristics.text =
+                                    "Characteristics: ${characteristic.uuid}"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        device.connectGatt(this, false, gattCallback)
     }
 
 }
--- app/src/main/java/com/prabhakar/pointoassignment/MainActivity.kt
+++ app/src/main/java/com/prabhakar/pointoassignment/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.prabhakar.pointoassignment
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
+import com.google.android.gms.tasks.Task
 import com.prabhakar.pointoassignment.databinding.ActivityMainBinding
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -18,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_CODE = 10001
+        const val REQUEST_CHECK_SETTINGS = 10004
+
     }
 
     private val backgroundLocationPermission =
@@ -57,12 +71,16 @@ class MainActivity : AppCompatActivity() {
 
         service = Intent(this, LocationService::class.java)
 
+        if (!isLocationEnabled(this)) {
+            enableGPS(this)
+        }
+        checkBluetoothPermission()
         binding.btnStart.setOnClickListener {
             checkLocationPermissions()
         }
 
         binding.btnBLE.setOnClickListener {
-            checkBluetoothPermission()
+            startActivity(Intent(this, BLEActivity::class.java))
         }
 
     }
@@ -136,7 +154,7 @@ class MainActivity : AppCompatActivity() {
         ) {
             requestBluetoothPermissions()
         } else {
-            startActivity(Intent(this, BLEActivity::class.java))
+//            startActivity(Intent(this, BLEActivity::class.java))
         }
     }
 
@@ -170,6 +188,39 @@ class MainActivity : AppCompatActivity() {
                     it == PackageManager.PERMISSION_GRANTED
                 }) {
                 Toast.makeText(this, "All Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isLocationEnabled(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    fun enableGPS(activity: Activity) {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+            .build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val settingsClient: SettingsClient = LocationServices.getSettingsClient(activity)
+        val task: Task<LocationSettingsResponse> =
+            settingsClient.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied
+            // You can proceed with location requests
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    exception.startResolutionForResult(activity, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
             }
         }
     }
